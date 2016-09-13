e6c677e955e999bb4a438771bffeb035dc296ebb
ChrisVbot <chris.veillette@gmail.com>
2016-09-13 22:23:09 +0000

    pushing post changes

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,3 +1,28 @@
 $('.close-block-profile').on('click', function(){
   $('.block-profile').animate({"top":"100%", "bottom":"-100%"});
 });
+
+$(".documentTitle").on("keypress", function(e){
+  var thisDoc = $(this);
+  var docId = thisDoc.parent().data("live-code");
+  var newTitle = thisDoc.html();
+  if(e.keyCode === 13){
+    e.preventDefault();
+    thisDoc.next().focus();
+    $.ajax({
+      method: 'POST',
+      url: '/profile/rename/',
+      data: {
+        docId: docId,
+        newTitle: newTitle
+      },
+      success: function(data){
+        console.log('Successfully posted');
+        thisDoc.blur().next().focus();
+        return false;
+      }
+    })
+  }else{
+    console.log("Nothing registered");
+  }
+});
